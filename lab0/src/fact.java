b779896ea15c4a754296deb47d3bfef539a89e8d
Piotr Stróżyk <93133284+piotrstrozyk@users.noreply.github.com>
2024-03-24 14:48:53 +0100

    Tutorial

---
+++ lab0/src/fact.java
@@ -0,0 +1,58 @@
+package lab0.src;
+
+import java.util.Scanner;
+
+public class fact {
+    public static void main (String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n;
+        Scanner menu = new Scanner(System.in);
+        int m = 1;
+        while (m == 1) {
+            System.out.println("Choose: 0-Exit   1-calculating factorial for n   2-calculate 2 to nth power");
+            m = menu.nextInt();
+            switch(m){
+                case 0:
+                    break;
+                case 1:
+                    System.out.println("Enter a number: ");
+                    n = reader.nextInt();
+                    if(n >= 0){
+                        
+                        long silnia = calcFactorial(n);
+                        System.out.println("Factorial of " + n + " is " + silnia);
+                    }
+                case 2:
+                    System.out.println("Enter a number: ");
+                    n = reader.nextInt();
+                    if(n >= 0){
+                        long power = calcPower(n);
+                        System.out.println("2 to the power of " + n + " is " + power);
+                    }
+                    
+            }
+
+        }
+        reader.close();
+        menu.close();
+}
+    public static long calcFactorial(int n) {
+        long result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+    public static long calcPower(int n) {
+        long result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= 2;
+        }
+        return result;
+    }
+}
+
+//Run:  java lab0.src.fact
+//Compile: javac fact.java
